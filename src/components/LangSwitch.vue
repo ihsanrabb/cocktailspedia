4e8cac2e10d401dfdc4ebdbdd9899c803aaa50c9
Ihsan <ihsan@MacBook-Pro-Ihsan.local>
2020-06-25 21:06:38 +0700

    create lang switch component

---
+++ src/components/LangSwitch.vue
@@ -0,0 +1,23 @@
+<template>
+    <select class="custom-select switch-select" @change="switchLocale($event)">
+        <option value="en" selected>Select Languange</option>
+        <option value="en">English</option>
+        <option value="de">German</option>
+    </select>
+
+</template>
+
+<script>
+export default {
+    name: 'LangSwitch',
+    methods: {
+        switchLocale(event) {
+            let locale = event.target.value
+            if(this.$i18n.locale !== locale) {
+                this.$i18n.locale = locale
+                window.localStorage.setItem('lang' , locale)
+            }
+        }
+    }
+}
+</script>
