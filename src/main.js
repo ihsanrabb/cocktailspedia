a28d04fa98ac22a6ef664ed4e1a69e0686fad97d
Ihsan <ihsan@MacBook-Pro-Ihsan.local>
2020-06-25 21:05:25 +0700

    setup vue i18n

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import 'popper.js';
 import 'bootstrap';
 import "../src/assets/app.scss"
 import VueMeta from 'vue-meta'
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
 Vue.use(VueMeta)
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  runtimeCompiler: true,
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'de',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
